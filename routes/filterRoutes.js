9b736814125d208f9c2ba3d5ddcf2216bca2ad64
E V <evanvargas@gmail.com>
2023-11-15 11:44:46 -0600

    Need to new button logic

--- routes/filterRoutes.js
+++ routes/filterRoutes.js
@@ -6,13 +6,44 @@ const filterRoutes = express.Router();
 
 const getFirstRound = asyncHandler(async (req, res) => {
   const incoming = JSON.parse(req.params.incoming);
+  let currentPage = 1;
+  const limit = 10;
 
-  if (Object.values(incoming)[0] === "resN") {
-    const complaints = await Complaint.find({ resolved: false }).limit(10);
-    console.log(complaints.length);
+  if (Object.values(incoming)[0] === "resN" || Object.keys(incoming)[0] === "deux") {
+    if (Object.keys(incoming)[0] === "deux") {
+      currentPage = Object.values(incoming)[0];
+      
+      const complaints = await Complaint.aggregate([
+        { $match: { resolved: false } },
+        { $skip: (currentPage - 1) * limit },
+        { $limit: limit },
+      ]);
+
+      const totalComplaints = await Complaint.countDocuments({ resolved: false });
+
+      console.log("Complaint Count: " + complaints.length, "/ " + totalComplaints);
+
+      return res.status(200).json({
+        iteration: 1,
+        complaints,
+        currentPage,
+      });
+    }
+
+    const complaints = await Complaint.aggregate([
+      { $match: { resolved: false } },
+      { $skip: (currentPage - 1) * limit },
+      { $limit: limit },
+    ]);
+
+    const totalComplaints = await Complaint.countDocuments({ resolved: false });
+
+    console.log("Complaint Count: " + complaints.length, "/ " + totalComplaints);
+
     return res.status(200).json({
-      complaintNum: complaints.length,
+      iteration: 1,
       complaints,
+      currentPage,
     });
   } else if (Object.values(incoming)[0] === "resY") {
   }
